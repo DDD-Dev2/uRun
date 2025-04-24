604e2d9cf066cbd04db771dbced3600104e2d605
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-24 11:08:14 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,33 +1,34 @@
-// combined_script.js
-(function() {
+// combined_urun_execute.js
+(function () {
   'use strict';
 
-  // === execute_script.js contents ===
-  // (Insert that code here)
-
-  // === urun.js contents ===
+  // === urun.js ===
   document.addEventListener("keydown", function (e) {
     if (e.key == "~" && e.ctrlKey) {
       var t = window.open("", "_blank", "width=500,height=300");
-      var e = t.document.createElement("iframe");
-      (e.src = "//stunning-buttercream-ac72d8.netlify.app/popup.html"),
-      (e.style.cssText = "width:100%; height:100%; border:none;"),
-      t.document.body.appendChild(e),
-      t.document.title = "uRun",
+      var iframe = t.document.createElement("iframe");
+      iframe.src = "//stunning-buttercream-ac72d8.netlify.app/popup.html";
+      iframe.style.cssText = "width:100%; height:100%; border:none;";
+      t.document.body.appendChild(iframe);
+      t.document.title = "uRun";
+
       t.addEventListener("message", function (e) {
-        e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
+        if (e.data.toString().startsWith("execute:")) {
+          eval(e.data.toString().replace("execute:", ""));
+          t.close();
+        }
       });
     }
   });
 
+  // === execute_script.js ===
   window.addEventListener("keyup", event => {
     if (event.ctrlKey && event.which === 192) {
       let code = prompt("Eval:");
-      if (code.startsWith("javascript:")) {
+      if (code && code.startsWith("javascript:")) {
         code = code.substring(11);
       }
-      eval(code);
+      if (code) eval(code);
     }
   });
-
 })();
