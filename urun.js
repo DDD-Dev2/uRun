b375519caa34265bb3a49bb20782be7c70b0063d
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-24 12:51:31 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,25 +1,9 @@
-document.addEventListener('keydown', function (event) {
-  if (event.key === '~' && event.ctrlKey) {
-    const popup = window.open('', '_blank', 'width=500,height=300');
-    const iframe = popup.document.createElement('iframe');
-
-    iframe.src = '//stunning-buttercream-ac72d8.netlify.app/popup.html';
-    iframe.style.cssText = 'width:100%; height:100%; border:none;';
-    popup.document.body.appendChild(iframe);
-    popup.document.title = 'uRun';
-
-    popup.addEventListener('message', function (msgEvent) {
-      const data = msgEvent.data.toString();
-      if (data.startsWith('execute:')) {
-        eval(data.replace('execute:', ''));
-        popup.close();
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.altKey && event.key.toLowerCase() === "e") {
+      let code = prompt("Eval:");
+      if (code && code.startsWith("javascript:")) {
+        code = code.substring(11);
       }
-    });
-
-    let code = prompt('Eval:');
-    if (code && code.startsWith('javascript:')) {
-      code = code.substring(11);
+      if (code) eval(code);
     }
-    if (code) eval(code);
-  }
-});
+  });
