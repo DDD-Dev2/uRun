c39377153621f44bec38e8435171118058ccfd11
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-24 11:02:21 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,25 +1,33 @@
-/// urun.js
-document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "//stunning-buttercream-ac72d8.netlify.app/popup.html"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
-			});
-	}
-});
+// combined_script.js
+(function() {
+  'use strict';
 
-/// execute_script.js
-window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.which === 192) {
-    let code = prompt("Eval:");
-    if (code.startsWith("javascript:")) {
-      code = code.substring(11);
+  // === execute_script.js contents ===
+  // (Insert that code here)
+
+  // === urun.js contents ===
+  document.addEventListener("keydown", function (e) {
+    if (e.key == "~" && e.ctrlKey) {
+      var t = window.open("", "_blank", "width=500,height=300");
+      var e = t.document.createElement("iframe");
+      (e.src = "//stunning-buttercream-ac72d8.netlify.app/popup.html"),
+      (e.style.cssText = "width:100%; height:100%; border:none;"),
+      t.document.body.appendChild(e),
+      t.document.title = "uRun",
+      t.addEventListener("message", function (e) {
+        e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
+      });
     }
-    eval(code);
-  }
-});
+  });
+
+  window.addEventListener("keyup", event => {
+    if (event.ctrlKey && event.which === 192) {
+      let code = prompt("Eval:");
+      if (code.startsWith("javascript:")) {
+        code = code.substring(11);
+      }
+      eval(code);
+    }
+  });
+
+})();
