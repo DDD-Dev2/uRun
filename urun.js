0cd121551f5690aeecef47036ba879ef67314613
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-24 10:52:41 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -12,6 +12,8 @@ document.addEventListener("keydown", function (e) {
 			});
 	}
 });
+
+/// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
     let code = prompt("Eval:");
