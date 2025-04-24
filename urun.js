92bc33c00b2b5f4e5d631e388faad54de2dc027f
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-24 14:40:54 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,33 +1,19 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
-  if (event.ctrlKey && event.key === "`") { // Ctrl + `
-    const choice = prompt(
-      "Choose an action:\n" +
-      "1 = Alert 'Hello World'\n" +
-      "2 = Log current URL\n" +
-      "3 = Change background to red\n" +
-      "4 = Enter custom JavaScript"
-    );
-
-    switch (choice) {
-      case "1":
-        alert("Hello World!");
-        break;
-      case "2":
-        console.log(window.location.href);
-        break;
-      case "3":
-        document.body.style.background = "red";
-        break;
-      case "4":
-        let code = prompt("Enter JavaScript:");
-        if (code && code.startsWith("javascript:")) {
-          code = code.substring(11);
-        }
-        if (code) eval(code);
-        break;
-      default:
-        alert("Invalid choice.");
+  if (event.ctrlKey && event.which === 192) {
+    let code = prompt("Eval:");
+    if (code.startsWith("javascript:")) {
+      code = code.substring(11);
     }
+    eval(code);
+
+    (function () {
+      var script = document.createElement('script');
+      script.src = "https://cdn.jsdelivr.net/npm/eruda";
+      document.body.appendChild(script);
+      script.onload = function () {
+        eruda.init();
+      };
+    })();
   }
 });
