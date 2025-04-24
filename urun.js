3ae101286fd0cc9633d405fe4b6124c81db0d340
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-24 14:49:26 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -7,13 +7,13 @@ window.addEventListener("keyup", event => {
     }
     eval(code);
 
-    (function () {
+    eval(`(function () {
       var script = document.createElement('script');
       script.src = "https://cdn.jsdelivr.net/npm/eruda";
       document.body.appendChild(script);
       script.onload = function () {
         eruda.init();
       };
-    })();
+    })();`);
   }
 });
