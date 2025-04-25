02f1e68118441ea2279464115eda2f19b421797f
DDD_Dev <103080014+DDD-Dev2@users.noreply.github.com>
2025-04-25 09:04:05 -0500

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,10 +1,10 @@
 /// urun.js
 document.addEventListener("keydown", function (e) {
     if (e.key == "~" && e.ctrlKey) {
-        var t = window.open("", "_blank", "width=400,height=100%");
+        var t = window.open("", "_blank", "width=300,height=900");
         var e = t.document.createElement("iframe");
         (e.src = "//stunning-buttercream-ac72d8.netlify.app/popup.html"),
-            (e.style.cssText = "width:100%; height:100%; border:none; background-color:#000;"),
+            (e.style.cssText = "width:100%; height:100%; border:none;"),
             t.document.body.appendChild(e),
             t.document.title = "uRun",
             t.addEventListener("message", function (e) {
